f79156b52696d72d61658a528bc2444d7b0ea3d7
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-11-02 23:45:21 +0530

    first

--- src/components/MainContainer/VideoContainer.jsx
+++ src/components/MainContainer/VideoContainer.jsx
@@ -4,12 +4,13 @@ import { useSelector } from "react-redux";
 export default function VideoContainer() {
   // const TrailerIds = useSelector((store) => store.movies?.nowPlayingMovies);
   const keyData = useSelector((store) => store.movies?.nowPlayingTrailer);
+  const autoPlay = "0";
 
   return (
     <>
       <iframe
         className="w-screen aspect-video"
-        src={`https://www.youtube.com/embed/${keyData?.key}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0`}
+        src={`https://www.youtube.com/embed/${keyData?.key}?autoplay=${autoPlay}&mute=1&controls=0&showinfo=0&rel=0`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
